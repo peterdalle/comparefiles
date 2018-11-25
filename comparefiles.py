c8328f628cf5ceb577865ebdc4ab46fbd209b813
peterdalle <peterdalle@gmail.com>
2018-11-25 13:32:24 +0100

    Reformatted function names

--- comparefiles.py
+++ comparefiles.py
@@ -1,33 +1,32 @@
-cd # encoding: utf-8
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 import os
 import sys
 import hashlib
 
 def main(fileextension: str, directory=""):     
     """Main program."""
-    # Parse directory.
-    parseddirectory = parsedirectory(directory)
+    parseddirectory = parse_directory(directory)
     if parseddirectory != "":
         print("Searching {} in {}...".format(fileextension, parseddirectory))
 
         # Get list of all files.
-        files, searchscope = getallfiles(fileextension, parseddirectory, recursive=True)
+        files, searchscope = get_all_files(fileextension, parseddirectory, recursive=True)
         if searchscope == "subfolders":
             print("Searched {} file(s) in all subdirectories".format(len(files)))
         else:
             print("Searched {} file(s) in current directory".format(len(files)))
 
         # Compare all files and get list of identical files.
-        matchedfiles = findidentical(files)
-        print("Found {} matches across {} files".format(countuniquehashes(matchedfiles), len(matchedfiles)))
+        matchedfiles = find_identical_files(files)
+        print("Found {} matches across {} files".format(count_unique_hashes(matchedfiles), len(matchedfiles)))
 
-        # Display list of identical files.
-        displayidenticalfiles(matchedfiles, fullfilenames=True, groupfiles=True)
+        display_identical_files(matchedfiles, fullfilenames=True, groupfiles=True)
     else:
         # Couldn't parse directory.
         print("Directory not found:", directory)
 
-def parsedirectory(directory: str) -> str:
+def parse_directory(directory: str) -> str:
     """Check for valid directory name. If empty, use current directory. If directory does not exist, return empty string."""
     if directory == "":
         return(os.getcwd())
@@ -37,7 +36,7 @@ def parsedirectory(directory: str) -> str:
             return(os.path.join(directory))
     return("")
 
-def getallfiles(fileextension: str, directory: str, recursive=True) -> (list, str):
+def get_all_files(fileextension: str, directory: str, recursive=True) -> (list, str):
     """Get all matching files in current directory and subdirectories and their MD5 hash."""
     l = []
     if recursive:
@@ -47,14 +46,20 @@ def getallfiles(fileextension: str, directory: str, recursive=True) -> (list, st
             for file in files:
                 if fileextension in file:
                     filename = os.path.join(rootdir, file)
-                    l.append({"filename": file, "md5": md5(filename), "absolutefilename": filename, "relativefilename": filename.replace(directory, "")})
+                    l.append({"filename": file, 
+		                    	"md5": md5(filename), 
+		                    	"absolutefilename": filename, 
+		                    	"relativefilename": filename.replace(directory, "")})
     else:
         # Search files only in current directory.
         searchscope = "folder"
         for file in os.listdir(directory):          
             if fileextension in file:
                 filename = os.path.join(directory, file)
-                l.append({"filename": file, "md5": md5(filename), "absolutefilename": filename, "relativefilename": filename})
+                l.append({"filename": file, 
+                	"md5": md5(filename), 
+                	"absolutefilename": filename, 
+                	"relativefilename": filename})
     return(l, searchscope)
 
 def md5(filename: str) -> str:
@@ -68,24 +73,24 @@ def md5(filename: str) -> str:
         print("Couldn't read {}".format(filename))
     return md5hash.hexdigest()
 
-def findidentical(files: list) -> int:
+def find_identical_files(files: list) -> int:
     """Get a list of all files where their MD5 hashes match. This is slow (n*n), but easy to do..."""
     l = []
     for currentfile in files:
         for comparefile in files:
             if (currentfile["md5"] == comparefile["md5"]) and (currentfile["absolutefilename"] != comparefile["absolutefilename"]):
                 l.append(currentfile)
-    return(removeduplicatedicts(l))
+    return(remove_duplicate_dicts(l))
 
-def removeduplicatedicts(lst: list) -> list:
+def remove_duplicate_dicts(lst: list) -> list:
     """Remove duplicate dictionaries from a list."""
     return([dict(t) for t in {tuple(d.items()) for d in lst}])
 
-def countuniquehashes(lst: list) -> int:
+def count_unique_hashes(lst: list) -> int:
     """Count the number of unique hashes in list with dictionaries."""
     return(len(set([(lambda k: k["md5"])(k) for k in lst])))
 
-def displayidenticalfiles(files: list, fullfilenames=True, groupfiles=True):
+def display_identical_files(files: list, fullfilenames=True, groupfiles=True):
     """Display files that are identical."""
     if len(files) > 0:
         # Sort files by hash to group them together when printing.
@@ -101,13 +106,10 @@ def displayidenticalfiles(files: list, fullfilenames=True, groupfiles=True):
             last_hash = file["md5"]
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        if len(sys.argv) > 2:
-            # File extension and folder.
-            main(sys.argv[1], sys.argv[2])
-        else:
-            # File extension.
-            main(sys.argv[1])
+    if len(sys.argv) > 2:
+        main(fileextension=sys.argv[1], directory=sys.argv[2])
+    elif len(sys.argv) > 1:
+        main(fileextension=sys.argv[1])
     else:
         print("Please supply a parameter for a file extension to search for (or . to search all files).")
         print("Usage:   python {} [extension] [folder]".format(sys.argv[0]))
